36698a7bc0be6cd2657622ab756d45216cb44b20
Muhammad Ramdani <111719771+SweetzDreamz@users.noreply.github.com>
2024-12-28 02:10:47 +0700

    Add files via upload

---
+++ template/jumbotron.php
@@ -0,0 +1,12 @@
+<!--jumbotron-->
+<div class="jumbotron">
+<div class="text-center">
+    <h1 class="fw-bold">ANTARA BUKU, TULISAN, <br> DAN HARAPAN</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead ">Mari membaca walau hanya sebentar</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <a href="?p=view-all&s=buku"><button type="button" class="btn btn-primary btn-lg px-4 gap-3 rounded-pill">Mulai Membaca</button></a>
+      </div>
+    </div>
+  </div>
+</div>
